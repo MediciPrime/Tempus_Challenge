84522b6a60c15cccfdf95c77f80637b7108b2261
Behram Radmanesh <behram11@gmail.com>
2017-03-20 13:50:50 -0400

    use RO and AO for variant read depth and percent calculations

--- vcf_parser.py
+++ vcf_parser.py
@@ -30,32 +30,34 @@ def vcf_parser(file_location):
                     ref = uInfo[3]
                     alt = uInfo[4]
                     
-                    # obtain correct information w/ try except blocks
-                    try:
-                        AB = float(re.search('AB=0(\.\d+)?|1\.0', uInfo[7]).group(0).rsplit('=')[1])
-                    except Exception:
-                        AB = 'NA'
-                        
-                    try:
-                        DP = int(re.search('DP=[1-9]+', uInfo[7]).group(0).rsplit('=')[1])
-                    except Exception:
-                        DP = 'NA'
-                    
+                    # obtain the left most alternative allele type
                     try:
                         TYPE = re.search('TYPE=[a-z]+', uInfo[7]).group(0).rsplit('=')[1]
                     except Exception:
                         TYPE = 'NA'
-
+                        
                     # calculate number of variant supporting reads
                     try:
-                        #VSR = re.search('AO=[1-9]+', uInfo[7]).group(0).rsplit('=')[1]
-                        VSR = int(DP - (AB * DP))
+                        #VSR = sum(re.search('AO=[1-9]+', uInfo[7]).group(0).rsplit('=')[1].rsplit(','))
+                        VSR = int(re.search(';AO=[1-9]+', uInfo[7]).group(0).rsplit('=')[1].rsplit(',')[0])
                     except Exception:
                         VSR = 'NA'
 
+                    # calculate number of reference allele observations
+                    try:
+                        RO = int(re.search(';RO=[0-9]+', uInfo[7]).group(0).rsplit('=')[1])
+                    except:
+                        RO = 'NA'
+
+                    # calculate total read depth at variant location
+                    try:
+                        TRD = RO + sum(list(map(int, re.search(';AO=[0-9]+', uInfo[7]).group(0).rsplit('=')[1].rsplit(','))))
+                    except:
+                        TRD = 'NA'
+                        
                     # calculate percent of variant supporting reads
                     try:
-                        PVSR = 1 - AB
+                        PVSR = round((VSR / TRD)*100, 2)
                     except Exception:
                         PVSR = 'NA'
                     
@@ -82,8 +84,8 @@ def vcf_parser(file_location):
                         affected_gene = 'NA'
 
                     # insert parsed information into 'table.txt'
-                    w.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\n'.format(
-                        chrom, pos, ref, alt, TYPE, DP, VSR, PVSR, allele_freq, affected_gene))
+                    w.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}%\t{8}\t{9}\n'.format(
+                        chrom, pos, ref, alt, TYPE, TRD, VSR, PVSR, allele_freq, affected_gene))
 
 
 # Allow users to specify the location of VCF file
